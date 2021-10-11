e9ebb5b4ab0cd4af1a91316d9278974fc7c7ff4e
Lobo <matheuscardoso1@poli.ufrj.br>
2021-10-10 23:49:17 -0300

    made first scraper, that gets all articles from a search page

---
+++ python_medium_articles/index.py
@@ -0,0 +1,35 @@
+from json import encoder
+import requests
+import json
+from bs4 import BeautifulSoup
+
+endpoint = "https://www.tecmundo.com.br/busca?q=python"
+
+source = requests.get(endpoint).content
+
+soup = BeautifulSoup(source, 'lxml')
+
+articles = soup.find_all('article', class_='tec--card--medium')
+
+results = []
+
+for article in articles:
+    image = article.find('img')['data-src']
+    title = str(article.find('a', class_="tec--card__title__link").text)
+    url = article.find('a',class_="tec--card__title__link")['href']
+    article_obj = {
+        'image': image,
+        'title': title,
+        'url': url
+    }
+    results.append(article_obj)
+
+for article_obj in results:
+    print(article_obj)
+
+# Salvando num arquivo json
+json_results = json.dumps(results,  ensure_ascii=False).encode('utf-8')
+json_file = open('scraped_articles.json', 'w')
+json_file.write(json_results.decode())
+json_file.close()
+
---
+++ tests/test_python_medium_articles.py
@@ -0,0 +1,5 @@
+from python_medium_articles import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
